26c9eb4ea10ed87aa35462c15ea0a5150f6c70ed
Tixxy-A <118925538+Tixxy-A@users.noreply.github.com>
2025-02-13 11:39:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,14 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://660c1746a0e0f10008ef5e49--cloud-brooker.netlify.app", // Your frontend URL
+    credentials: true, // Allow cookies (if using authentication)
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  })
+);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/provider", providerRouter);
 app.use("/api/v1/admin", adminRouter);
